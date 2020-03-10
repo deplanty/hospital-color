006ead27d82200dac9acba877aafed91eb46c8f4
deplanty <38189611+deplanty@users.noreply.github.com>
2020-03-10 18:49:35 +0100

    Change text color when dark background

--- assets/js/trigram.js
+++ assets/js/trigram.js
@@ -1,4 +1,5 @@
 function removeFields() {
+  // Remove all lines from the view
   let col_1 = document.getElementById("container-0");
   let col_2 = document.getElementById("container-1");
 
@@ -10,6 +11,7 @@ function removeFields() {
 }
 
 function addField(trigram) {
+  // Add a line to the view
   let container = document.getElementById("container-" + n_trigram % 2);
   document.getElementById("trigram-input").focus();
   let hexa = ".";
@@ -51,6 +53,7 @@ function addField(trigram) {
   input.style.background = hexa;
   input.value = hexa_rgb;
   input.disabled = true;
+  input.style.color = colorFromBackground(hexa);
   bloc.appendChild(input);
   container.appendChild(bloc);
 
@@ -59,6 +62,7 @@ function addField(trigram) {
 }
 
 function actionInput() {
+  // Free text -> hexa + rgb
   let trigram = document.getElementById("trigram-input");
   addField(trigram.value);
   trigram.value = "";
@@ -66,13 +70,23 @@ function actionInput() {
 }
 
 function actionSelectMenu() {
+  // Hospital -> hexa + rgb
   let select = document.getElementById("trigram-select-menu");
   let trigram = select.options[select.selectedIndex]
   addField(trigram.value);
   select.selectedIndex = 0;
 }
 
+function fromText(text) {
+  // ZAZxxx -> #FF00FF
+  let hexa = "#";
+  let tri = text.slice(0, 3);
+
+  return fromTrigram(tri);
+}
+
 function fromTrigram(tri) {
+  // ZAZ -> #FF00FF
   let hexa = "#";
   let letter = "";
   for (i = 0; i < 3; i++) {
@@ -86,36 +100,57 @@ function fromTrigram(tri) {
   return hexa;
 }
 
-function fromText(text) {
-  let hexa = "#";
-  let tri = text.slice(0, 3);
-
-  return fromTrigram(tri);
-}
-
 function letterToHexa(letter) {
+  // A -> 00, Z -> FF
   let base_36 = parseInt(letter, 36);
   let base_26 = base_36 - 10;  // No number allowed
-  if (base_26 < 0){
-    alert("Aucun chiffre n'est accepté");
+  if (isNaN(base_26)) {
+    return ".";
+  } else if (base_26 < 0){
     return ".";
   }
   let normalize = Math.round(255 * base_26 / 25);  // [255, 0]
-  normalize = normalize.toString(16);
-  if (normalize.length == 1) {
-    normalize = "0" + normalize;
+  return intToHexa(normalize);
+}
+
+function intToHexa(value) {
+  value = value.toString(16);
+  if (value.length == 1) {
+    value = "0" + value;
   }
-  return normalize;
+  return value
 }
 
 function hexaToRGB(hexa) {
-  let r = hexa.slice(1, 3);
-  let g = hexa.slice(3, 5);
-  let b = hexa.slice(5);
+  // #FF00FF -> rgb(255, 0, 255)
+  let r = parseInt(hexa.slice(1, 3), 16);
+  let g = parseInt(hexa.slice(3, 5), 16);
+  let b = parseInt(hexa.slice(5), 16);
 
-  r = parseInt(r, 16);
-  g = parseInt(g, 16);
-  b = parseInt(b, 16);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
-  return "rgb(" + r + ", " + g + ", " + b + ")";
+function colorFromBackground(hexa) {
+  // FF00FF -> 00FF00
+  let r = parseInt(hexa.slice(1, 3), 16);
+  let g = parseInt(hexa.slice(3, 5), 16);
+  let b = parseInt(hexa.slice(5), 16);
+
+  if ((r+g+b)/3 >= 122.5) {
+    return "#101010";
+  } else {
+    return "#e0e0e0";
+  }
+}
+
+function setOptionsInSelect(list_trigrams) {
+  // Put the list of trigrams in the select element
+  let select = document.getElementById("trigram-select-menu");
+  for (let hospital in list_trigrams) {
+    let opt = document.createElement("option");
+    let text = document.createTextNode(hospital);
+    opt.value = list_trigrams[hospital];
+    opt.appendChild(text);
+    select.appendChild(opt);
+  }
 }
