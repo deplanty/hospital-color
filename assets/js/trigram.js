c579c8c2fbdeb6d70e9d890e1c21e5ca66cb78d6
deplanty <38189611+deplanty@users.noreply.github.com>
2020-03-10 17:31:28 +0100

    Set the hospitals in a json file

--- assets/js/trigram.js
+++ assets/js/trigram.js
@@ -1,6 +1,6 @@
 function removeFields() {
-  var col_1 = document.getElementById("container-0");
-  var col_2 = document.getElementById("container-1");
+  let col_1 = document.getElementById("container-0");
+  let col_2 = document.getElementById("container-1");
 
   col_1.innerHTML = "";
   col_2.innerHTML = "";
@@ -10,15 +10,16 @@ function removeFields() {
 }
 
 function addField(trigram) {
-  var container = document.getElementById("container-" + n_trigram % 2);
+  let container = document.getElementById("container-" + n_trigram % 2);
   document.getElementById("trigram-input").focus();
+  let hexa = ".";
   // Verify input
   if (trigram.length < 3) {
     return;
   } else if (trigram.length == 3) {
-    var hexa = fromTrigram(trigram);
+    hexa = fromTrigram(trigram);
   } else if (trigram.length > 3) {
-    var hexa = fromText(trigram);
+    hexa = fromText(trigram);
   }
   // Get hexa code
   if (hexa == "."){
@@ -26,25 +27,25 @@ function addField(trigram) {
   }
 
   // Append a new line
-  var form = document.createElement("form");
+  let form = document.createElement("form");
   form.className = "form-inline";
-  var bloc = document.createElement("div");
+  let bloc = document.createElement("div");
   bloc.className = "input-group mb-3";
-  var div = document.createElement("div");
+  let div = document.createElement("div");
   div.className = "input-group-prepend";
-  var span = document.createElement("span");
+  let span = document.createElement("span");
   span.className = "input-group-text";
   span.style.fontFamily = "Consolas";
-  var text = document.createTextNode(trigram);
+  let text = document.createTextNode(trigram);
   span.appendChild(text);
   div.appendChild(span);
   bloc.appendChild(div);
   form.appendChild(bloc);
 
-  var rgb = hexaToRGB(hexa);
-  var hexa_rgb = hexa + " - " + rgb;
+  let rgb = hexaToRGB(hexa);
+  let hexa_rgb = hexa + " - " + rgb;
 
-  var input = document.createElement("input");
+  let input = document.createElement("input");
   input.type = "text";
   input.className = "form-control";
   input.style.background = hexa;
@@ -58,22 +59,22 @@ function addField(trigram) {
 }
 
 function actionInput() {
-  var trigram = document.getElementById("trigram-input");
+  let trigram = document.getElementById("trigram-input");
   addField(trigram.value);
   trigram.value = "";
   trigram.focus();
 }
 
 function actionSelectMenu() {
-  var select = document.getElementById("trigram-select-menu");
-  var trigram = select.options[select.selectedIndex]
+  let select = document.getElementById("trigram-select-menu");
+  let trigram = select.options[select.selectedIndex]
   addField(trigram.value);
   select.selectedIndex = 0;
 }
 
 function fromTrigram(tri) {
-  var hexa = "#";
-  var letter = "";
+  let hexa = "#";
+  let letter = "";
   for (i = 0; i < 3; i++) {
     letter = letterToHexa(tri[i]);
     if (letter == "."){
@@ -86,20 +87,20 @@ function fromTrigram(tri) {
 }
 
 function fromText(text) {
-  var hexa = "#";
-  var tri = text.slice(0, 3);
+  let hexa = "#";
+  let tri = text.slice(0, 3);
 
   return fromTrigram(tri);
 }
 
 function letterToHexa(letter) {
-  var base_36 = parseInt(letter, 36);
-  var base_26 = base_36 - 10;  // No number allowed
+  let base_36 = parseInt(letter, 36);
+  let base_26 = base_36 - 10;  // No number allowed
   if (base_26 < 0){
     alert("Aucun chiffre n'est accepté");
     return ".";
   }
-  var normalize = Math.round(255 * base_26 / 25);  // [255, 0]
+  let normalize = Math.round(255 * base_26 / 25);  // [255, 0]
   normalize = normalize.toString(16);
   if (normalize.length == 1) {
     normalize = "0" + normalize;
@@ -108,9 +109,9 @@ function letterToHexa(letter) {
 }
 
 function hexaToRGB(hexa) {
-  var r = hexa.slice(1, 3);
-  var g = hexa.slice(3, 5);
-  var b = hexa.slice(5);
+  let r = hexa.slice(1, 3);
+  let g = hexa.slice(3, 5);
+  let b = hexa.slice(5);
 
   r = parseInt(r, 16);
   g = parseInt(g, 16);
